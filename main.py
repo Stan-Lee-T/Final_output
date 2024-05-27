74b0603062b3b4f3a33be12dfa81815d061e6274
Jean Dwei Barry <79318522+Jeandweibarry@users.noreply.github.com>
2024-05-27 23:57:37 +0800

    main.py

--- main.py
+++ main.py
@@ -16,12 +16,7 @@ class GUI:
         self.main_frame = None
         self.content_frame = tk.Frame(root, bg="gray")
         self.content_frame.pack(fill="both", expand=True)
-
-    def Exit(self):
-        exit()
-
-    def login(self):
-        print("Login")
+        self.con = sqlite3.connect("C:\\Users\\Jeff\\PycharmProjects\\FinalOutput\\register_account.db")
 
     def create_account(self):
         account_registration = Create_account.account_registration()
@@ -36,13 +31,6 @@ class GUI:
         self.main_frame = tk.LabelFrame(self.content_frame, width=900, height=500, relief="raised", borderwidth=10, bg="#CDCDCD", text="L O G I N", font=("arial", 30, "bold"))
         self.main_frame.place(x=500, y=250)
 
-    def buttons(self):
-        self.exit_button = tk.Button(self.main_frame, width=15, height=2, text="C A N C E L", font=("arial", 12, "bold"), command=self.Exit, bg="#C5C6C7", relief="raised", borderwidth=10)
-        self.exit_button.place(y=350, x=650)
-        self.login_button = tk.Button(self.main_frame, width=15, height=2, text="L O G I N", font=("arial", 12, "bold"), command=self.login, bg="#C5C6C7", relief="raised", borderwidth=10)
-        self.login_button.place(y=350, x=425)
-        self.create_account_button = tk.Button(self.content_frame, width=30, height=2, text="C R E A T E  A N  A C C O U N T", font=("arial", 12, "bold"), command=self.create_account, bg="gray", relief="flat", borderwidth=0)
-        self.create_account_button.place(y=750, x=1025)
 
     def labels(self):
         self.username_label = tk.Label(self.main_frame, text="Username", font=("arial", 20), bg="#CDCDCD")
@@ -55,6 +43,27 @@ class GUI:
         self.username_entry.place(y=85, x=50)
         self.password_entry = tk.Entry(self.main_frame, width=50, show="*", font=("arial", 20))
         self.password_entry.place(y=235, x=50)
+
+    def Exit(self):
+        exit()
+
+    def login(self):
+        cursor1 = self.con.cursor()
+        cursor1.execute("SELECT * FROM registered WHERE username = '" + self.username_entry.get() + "' AND password = '" + self.password_entry.get() + "'")
+        result = cursor1.fetchone()
+        if result == (self.username_entry.get(), self.password_entry.get()):
+            print("access granted")
+        self.con.commit()
+        self.con.close()
+
+    def buttons(self):
+        self.exit_button = tk.Button(self.main_frame, width=15, height=2, text="E X I T", font=("arial", 12, "bold"), command=self.Exit, bg="#C5C6C7", relief="raised", borderwidth=10)
+        self.exit_button.place(y=350, x=650)
+        self.login_button = tk.Button(self.main_frame, width=15, height=2, text="L O G I N", font=("arial", 12, "bold"), command=self.login, bg="#C5C6C7", relief="raised", borderwidth=10)
+        self.login_button.place(y=350, x=425)
+        self.create_account_button = tk.Button(self.content_frame, width=30, height=2, text="C R E A T E  A N  A C C O U N T", font=("arial", 12, "bold"), command=self.create_account, bg="gray", relief="flat", borderwidth=0)
+        self.create_account_button.place(y=750, x=1025)
+
 
 
 GUI = GUI()
